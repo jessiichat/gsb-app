306d3fbc8627ab5d374c601e7979358538b23f37
jessiichat <jessiichat@gmail.com>
2021-04-13 10:08:12 +0200

    Ajout composant navbar

---
+++ src/components/logo.vue
@@ -0,0 +1,17 @@
+<template>
+    <a href="index.html" class="logo">
+        <img class="logo" src="../assets/images/logo.svg">
+    </a>
+</template>
+
+<script>
+export default{
+    name: 'logo'
+}
+</script>
+
+<style scoped>
+    .logo {
+        height: 100%;
+    }
+</style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav class="flex">
+    <logo></logo>
+
+    <ul class="flex">
+        <li>
+            <a href="visiteurs.html">Visiteurs</a>
+        </li>
+        <li>
+            <a href="redacteurs.html">Rédacteurs</a>
+        </li>
+        <li>
+            <a href="chercheurs.html">Chercheurs</a>
+        </li>
+        <li>
+            <a href="addRapport.html">Ajout Rapport</a>
+        </li>
+        <li>
+            <a href="rapports.html">Rapports</a>
+        </li>
+        <li>
+            <a href="#" class="btn" id="signOutButton"> Déconnexion </a>
+        </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import logo from './logo.vue';
+
+export default{
+    name: 'navbar'
+}
+</script>
+
+<style scoped>
+    nav {
+    height: 10vh;
+    width: 90%;
+    margin: auto;
+    }
+
+    nav a {
+        color: white;
+    }
+</style>
